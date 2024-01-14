30e04d5c9efb77ef211dac807f585d64711384da
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-14 12:27:06 +0530

    Basic Setup and installation of required framework done

---
+++ app/(root)/rulings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Rulings</div>
+  )
+}
+
+export default page
---
+++ constants/index.ts
@@ -0,0 +1,78 @@
+import { SidebarLink } from "@/types";
+
+export const themes = [
+  { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
+  { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
+  { value: "system", label: "System", icon: "/assets/icons/computer.svg" },
+];
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Dashboard",
+  },
+  {
+    imgURL: "/assets/icons/users.svg",
+    route: "/rulings",
+    label: "Rulings",
+  },
+  {
+    imgURL: "/assets/icons/star.svg",
+    route: "/templates",
+    label: "Templates",
+  },
+  {
+    imgURL: "/assets/icons/suitcase.svg",
+    route: "/pendency",
+    label: "Pendency",
+  },
+  // {
+  //   imgURL: "/assets/icons/tag.svg",
+  //   route: "/tags",
+  //   label: "Tags",
+  // },
+  // {
+  //   imgURL: "/assets/icons/user.svg",
+  //   route: "/profile",
+  //   label: "Profile",
+  // },
+  // {
+  //   imgURL: "/assets/icons/question.svg",
+  //   route: "/ask-question",
+  //   label: "Ask a question",
+  // },
+  // {
+  //   imgURL: "/assets/icons/suitcase.svg",
+  //   route: "/dashboard",
+  //   label: "Dashboard",
+  // },
+];
+
+export const BADGE_CRITERIA = {
+  QUESTION_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  QUESTION_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  TOTAL_VIEWS: {
+    BRONZE: 1000,
+    SILVER: 10000,
+    GOLD: 100000,
+  },
+};

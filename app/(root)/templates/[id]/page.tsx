d8ad3d2187f55fc51732def935bd6217110a3906
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-01-28 17:53:51 +0530

    Just deplyoment23

--- app/(root)/templates/[id]/page.tsx
+++ app/(root)/templates/[id]/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-async-client-component */
 /* eslint-disable no-undef */
 // @ts-nocheck
-"use client"
+// "use client"
 
 import ParseHTML from '@/components/shared/ParseHTML';
 // import {CopyToClipboard} from 'react-copy-to-clipboard';
@@ -12,7 +12,7 @@ import { getTemplateById } from '@/lib/actions/template.action';
 
 import { auth } from '@clerk/nextjs';
 
-import {React, useState} from 'react'
+import {React} from 'react'
 
 const Page = async ({ params }) => {
   const { userId: clerkId } = auth();
@@ -24,24 +24,24 @@ const Page = async ({ params }) => {
   }
 
   const result = await getTemplateById({ templateId: params.id });
-  const [copied, setcopied] = useState(false);
+  // const [copied, setcopied] = useState(false);
 
-  const copyToClipboard = () => {
-    setcopied(true);
+  // const copyToClipboard = () => {
+  //   setcopied(true);
 
-    navigator.clipboard.writeText(result.descriptiion).then(
-      () => {
-        // console.log('copied');
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
+  //   navigator.clipboard.writeText(result.descriptiion).then(
+  //     () => {
+  //       // console.log('copied');
+  //     },
+  //     (err) => {
+  //       console.error(err);
+  //     }
+  //   );
 
-    setTimeout(() => {
-      setcopied(false);
-    }, 500);
-  };
+  //   setTimeout(() => {
+  //     setcopied(false);
+  //   }, 500);
+  // };
   return (
     <>
       <div className="flex-start w-full flex-col">
@@ -104,7 +104,7 @@ const Page = async ({ params }) => {
 
        <ParseHTML data={result.description} />;
 
-       {copied ? (
+       {/* {copied ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -135,7 +135,7 @@ const Page = async ({ params }) => {
             d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
           />
         </svg>
-      )}
+      )} */}
       {/* // eslint-disable-next-line no-undef
       <CopyToClipboard text={text} onCopy={(_text, result) => (result)}>
    <button>Copy</button>

81fb4816d80cb8e64555eb677e8f0c099939cf7c
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-02-04 22:59:22 +0530

    Just deplyoment51

--- lib/validations.ts
+++ lib/validations.ts
@@ -85,9 +85,9 @@ export const AnswerSchema = z.object({
 
 
 export const ProfileSchema = z.object({
-  name: z.string().min(5).max(50),
-  username: z.string().min(5).max(50),
-  bio: z.string().min(10).max(150),
-  portfolioWebsite: z.string().url(),
-  location: z.string().min(5).max(50),
+  name: z.string().min(2).max(50),
+  username: z.string().min(2).max(50),
+  section: z.string().min(2).max(150),
+  // portfolioWebsite: z.string().url(),
+  location: z.string().min(2).max(50),
 })

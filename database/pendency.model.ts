e32e3ab7a1c50fe9f61a9e594a2fa1c2fbcf6754
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-02-05 21:39:36 +0530

    Just deplyoment59

---
+++ database/pendency.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model, models, Document } from 'mongoose';
+
+export interface IPendency extends Document {
+  dak_no: string;
+  doc: string;
+  division: string;
+  c_no: string;
+  subject: string;
+  f_no: string;
+  dos: string;
+  dor: string;
+  remarks: string;
+  status: string;
+  author: Schema.Types.ObjectId;
+  createdOn: Date;
+}
+
+const PendencySchema = new Schema({
+  
+  dak_no: { type: String, required: true },
+  doc: { type: String },
+  division: { type: String, required: true},
+  c_no: { type: String, required: true },
+  subject: { type: String, required: true },
+  f_no: { type: String },
+  dos: { type: String },
+  dor: { type: String },
+  remarks: { type: String },
+  status: { type: String},
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdOn: { type: Date, default: Date.now },
+});
+
+const Pendency = models.Pendency || model('Pendency', PendencySchema);
+
+export default Pendency;

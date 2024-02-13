1d7a50a6685deec21f34b6e835738bf0f3becc07
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-02-13 22:36:33 +0530

    Just deplyoment103

--- components/task/data-table-row-actions.tsx
+++ components/task/data-table-row-actions.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const data-table-row-actions = () => {
+const data = () => {
   return (
     <div>data-table-row-actions</div>
   )
 }
 
-export default data-table-row-actions
+export default data

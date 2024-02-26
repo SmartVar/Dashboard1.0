06459dcc1b4de4fed382585be82c81e97d761617
SmartVar <130911742+SmartVar@users.noreply.github.com>
2024-02-26 23:33:53 +0530

    Update Modal.tsx

--- components/kanban/Modal/Modal.tsx
+++ components/kanban/Modal/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = (props) => {
   return (
     <div className={styles.modal} onClick={() => (props.onClose ? props.onClose() : "")}>
-      <div className={`${styles.modalContent} background-light900_dark300 light-border-2 text-dark300_light700 
+      <div className={`${styles.modalContent} custom-scroll background-light900_dark300 light-border-2 text-dark300_light700 
     flex w-full overflow-auto justify-between gap-4 rounded-lg sm:flex-row sm:items-center`} onClick={(event) => event.stopPropagation()}>
         {props.children}
       </div>

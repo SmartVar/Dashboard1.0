c516f4e891b0bb1f9c7277e2f7a6d09d5967a79a
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-06-28 21:13:51 +0530

    check

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -186,7 +186,14 @@ const Page = async ({ searchParams}: URLProps) => {
         badges={userInfo.badgeCounts}
       /> */}
       {/* <Overviewdash /> */}
-      <Fund />
+      <Fund params={{
+            id: ''
+          }} searchParams={{
+            q: undefined,
+            filter: undefined,
+            page: undefined,
+            dateParam: undefined
+          }} />
               </TabsContent>
             <TabsContent value="reports" className="text-dark200_light900 space-y-4 text-sm font-medium">
               <Report />

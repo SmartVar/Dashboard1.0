e235b1629d610d5d91c325d0053c824cf9b05046
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-06-30 20:45:55 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-{ /* import LeftSidebar from '@/components/shared/LeftSidebar'
-import RightSidebar from '@/components/shared/RightSidebar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+// import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
@@ -17,39 +17,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </section>
 
-        <RightSidebar /> 
-      </div>
-
-      <Toaster />
-    </main>
-  )
-}
-
-export default Layout */ }
-
-import React from 'react'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-// import RightSidebar from '@/components/shared/RightSidebar'
-import Navbar from '@/components/shared/navbar/Navbar'
-import MobileNav from '@/components/shared/navbar/MobileNav'
-import { Toaster } from '@/components/ui/toaster'
-import useMediaQuery from '@/components/shared/hooks/useMediaQuery'
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  const isMobile = useMediaQuery('(max-width: 767px)'); // Tailwind's md breakpoint
-
-  return (
-    <main className="background-light850_dark100 relative">
-      {isMobile ? <MobileNav /> : <Navbar />}
-
-      <div className="flex">
-        <LeftSidebar />
-
-        <section className="flex min-h-screen w-full flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <div className="mx-auto w-full max-w-5xl">
-            {children}
-          </div>
-        </section>
+        {/*  <RightSidebar /> */}
       </div>
 
       <Toaster />

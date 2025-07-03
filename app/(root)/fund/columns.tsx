f488e625f8161e7abf57fc669219e9326d1a8fd1
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-07-03 10:11:12 +0530

    Update columns.tsx

--- app/(root)/fund/columns.tsx
+++ app/(root)/fund/columns.tsx
@@ -187,10 +187,10 @@ export const columns: ColumnDef<FundDef>[] = [
         <ArrowUpDown className="ml-2 h-4 w-4" />
       </Button>
     ),
-    cell: ({ row }) => {
+    { /* cell: ({ row }) => {
       const value = row.getValue("tot_allot") as number;
       return `₹ ${value.toLocaleString("en-IN")}`;
-    },
+    }, */ }
   },
   {
     accessorKey: "balance",

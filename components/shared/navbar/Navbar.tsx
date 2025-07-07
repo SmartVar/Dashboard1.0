2785c4300151b686985bde0b804629dc9a0e7ead
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-07-07 16:54:55 +0530

    Update Navbar.tsx

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -12,13 +12,13 @@ const Navbar = () => {
     <nav className="flex-between background-light900_dark200 
     fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none 
     sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      { /* <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo1.svg"
           width={15}
           height={15}
           alt="PostalDashboard"
-        />
+        /> */ }
 
         <p className="h2-bold font-spaceGrotesk text-dark-100
          dark:text-light-900 max-sm:hidden">Postal <span className="text-primary-500">

7b36112b05adb0d707b4e6db6e46e084ce5a2ec9
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-07-10 20:44:03 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,51 +1,7 @@
-// import Link from "next/link"
 
-// import { Button } from "@/components/ui/button"
-{ /* import { Skeleton } from "@/components/ui/skeleton"
 
-const page = () => {
-  return (
-    <section>
-      <div className="flex w-full flex-col justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">Postal Dashboard</h1>
-        <div className="mt-3">
-            <h2 className="h2-bold text-dark100_light900">The Ultimate Office Utility Website for you</h2>
-            <p className="paragraph-regular text-dark200_light800">Kindly check the Dashboard for more information. Please use it for official purpose only</p>
-        {/* <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-           Edit Dashboard
-          </Button>
-        </Link>  
-      </div>
-
-      <div className="mb-12 mt-11 flex flex-wrap items-center justify-between gap-5">
-        <Skeleton className="h-14 flex-1" />
-        <div className="hidden max-md:block">
-          <Skeleton className="h-14 w-28" />
-        </div>
-      </div>
-
-      <div className="my-10 hidden flex-wrap gap-6 md:flex">
-        <Skeleton className="h-9 w-40" />
-        <Skeleton className="h-9 w-40" />
-        <Skeleton className="h-9 w-40" />
-        <Skeleton className="h-9 w-40" />
-      </div>
-
-      <div className="flex flex-col gap-6">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <Skeleton key={item} className="h-48 w-full rounded-xl" />
-        ))}
-      </div>
-      </div>
-    </section>
-  )
-}
-
-export default page */ }
-
-// import Link from "next/link"
-// import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 import Image from "next/image"
 
 const page = () => {

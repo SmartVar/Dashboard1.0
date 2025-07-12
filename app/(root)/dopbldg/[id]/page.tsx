c6fe80a6cd143d2b798f0a664f7473727e2d22c5
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-07-12 11:37:17 +0530

    Update page.tsx

--- app/(root)/dopbldg/[id]/page.tsx
+++ app/(root)/dopbldg/[id]/page.tsx
@@ -118,13 +118,15 @@ const Page = async ({ params }: ParamsProps) => {
         {/* At a Glance */}
         <Card className="col-span-1 lg:col-span-2 shadow-lg border border-gray-200 dark:border-gray-700 rounded-lg">
 <CardHeader className="flex items-center justify-between gap-2 border-b p-4 dark:border-gray-600">
-  <div className="flex items-center justify-between border-b p-4">
+  <div className="flex items-center gap-2">
     <Info className="text-blue-600 dark:text-blue-400" />
     <CardTitle className="text-lg font-semibold text-gray-900 dark:text-white">
       At a Glance
     </CardTitle>
   </div>
+  <div>
   <CopyButton text={result.po} />
+    </div>
 </CardHeader>
 
           <CardContent className="p-4 space-y-4">

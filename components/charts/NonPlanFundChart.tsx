4d7c19223074deff5711112cda0868d64fafca95
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-07-23 17:34:29 +0530

    Update NonPlanFundChart.tsx

--- components/charts/NonPlanFundChart.tsx
+++ components/charts/NonPlanFundChart.tsx
@@ -22,10 +22,10 @@ import { Label } from "@/components/ui/label"
 ChartJS.register(ArcElement, Tooltip, Legend, Title)
 
 const divisionData = {
-  Total: { allotment: 100, utilization: 48.86025 },
-  C-Sion: { allotment: 37.53, utilization: 32 },
-  E-Sion: { allotment: 30, utilization: 15 },
-  C-Pune: { allotment: 16, utilization: 1 },
+  "Total": { allotment: 100, utilization: 48.86025 },
+  "C-Sion": { allotment: 37.53, utilization: 32 },
+  "E-Sion": { allotment: 30, utilization: 15 },
+  "C-Pune": { allotment: 16, utilization: 1 },
   RO: { allotment: 10, utilization: 0.5 },
   NMD: { allotment: 1, utilization: 0 },
   THN: { allotment: 1, utilization: 0.24699 },
